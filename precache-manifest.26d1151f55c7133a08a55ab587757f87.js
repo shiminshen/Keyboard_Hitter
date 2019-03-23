1eee15c5af7a87b3bac31e095e46a0ce6b919ad7
shiminshen <smshen17@gmil.com>
2019-03-23 15:17:06 +0800

    Updates

---
+++ precache-manifest.26d1151f55c7133a08a55ab587757f87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d0fc7dbadc3f142c5d0",
+    "url": "/keyboard_hitter/static/js/runtime~main.df83ad85.js"
+  },
+  {
+    "revision": "ed756aca958dc548f032",
+    "url": "/keyboard_hitter/static/js/main.ef857143.chunk.js"
+  },
+  {
+    "revision": "21f1aa45bbeb7f6ba9ab",
+    "url": "/keyboard_hitter/static/js/2.acf98ebc.chunk.js"
+  },
+  {
+    "revision": "ed756aca958dc548f032",
+    "url": "/keyboard_hitter/static/css/main.74c28ae5.chunk.css"
+  },
+  {
+    "revision": "53b2efe62dcc162abd2d50ddc6cbb10b",
+    "url": "/keyboard_hitter/index.html"
+  }
+];
